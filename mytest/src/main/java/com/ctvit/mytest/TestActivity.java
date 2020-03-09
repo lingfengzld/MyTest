8c64c68fd602f7b4f2d5f1f8f84fcac46fd1ae21
zhanglei <zhanglei@ctvit.com.cn>
2020-03-09 15:19:47 +0800

    first commit to test

---
+++ mytest/src/main/java/com/ctvit/mytest/TestActivity.java
@@ -0,0 +1,12 @@
+package com.ctvit.mytest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
